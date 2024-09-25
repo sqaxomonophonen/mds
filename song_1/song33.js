deb799a2fcc63a7aa7af11045614de3249fcf921
Anders Kaare Straadt <anders@straadt.dk>
2024-09-25 03:03:36 +0200

    wip: welle

--- song_1/song33.js
+++ song_1/song33.js
@@ -1,22 +1,28 @@
 (_=>{
-	let SAMPLE_RATE=44100,
-	    N_CHANNELS=2,
-	    CHUNK_FRAMES=SAMPLE_RATE/10,
-	    N_CHUNKS=100,
-	    push=P(SAMPLE_RATE, N_CHANNELS, N_CHUNKS*CHUNK_FRAMES, "song3 - aks", "#fb1"),
-	    remaining=N_CHUNKS,
-	    phi_left=0,
-	    phi_right=0,
-	    dphi_left = 0.1,
-	    dphi_right = 0.11,
-	    X
-	    ;
+	let
+	N_CHANNELS=2,
+	CHUNK_FRAMES=@DEF(SR)/10,
+	N_CHUNKS=1000,
+	push=P(@DEF(SR), N_CHANNELS, N_CHUNKS*CHUNK_FRAMES, "song3 - aks", "#fb1"),
+	remaining=N_CHUNKS,
+	phi_left=0,
+	phi_right=0,
+	dphi_left = 0.1,
+	dphi_right = 0.11,
+	X
+	;
 
 	// XXX resolver tests
+	/*
 	console.log("PIIIII", @DEF(PI)); // shared global constant due to 2+ hits
 	console.log("PIIIII", @DEF(PI));
 	console.log("rate?", @DEF(SR)); // inlined
 	console.log("gain", @KIT(gain_node)); // kit/gain_node.js
+	*/
+
+	console.log("WELLE0");
+	let welle0 = @KIT(welle)(32,16);
+	console.log("WELLE1", welle0);
 
 	X = () => {
 		if (remaining-- <= 0) return;
@@ -33,7 +39,7 @@
 			while (phi_right > 2*Math.PI) phi_right -= 2*Math.PI;
 		}
 		push(xs);
-		setTimeout(X,10);
+		setTimeout(X,0);
 	};
 	X();
 })();
