3ee94b51d650b2604d0095711faf8a7ef51e2f4c
Anders Kaare Straadt <anders@straadt.dk>
2024-10-03 12:08:56 +0200

    better normalize

--- kit/sinc_fir.js
+++ kit/sinc_fir.js
@@ -2,14 +2,11 @@
 	let half_size = ratio*n_zero_crossings;
 	let fir_size = 2*half_size - 1;
 	let coef = new Float32Array(fir_size);
-	let sum = 0;
 	let j;
 	for (let i = 0; i < fir_size; i++) {
 		j = (i-half_size+1);
 		let x = (j*Math.PI)/ratio;
-		coef[i] = (x==0 ? 1 : ((i+1)%ratio==0) ? 0 : (Math.sin(x)/x)) * window_fn(j/half_size);
-		sum += coef[i];
+		coef[i] = ((x==0 ? 1 : ((i+1)%ratio==0) ? 0 : (Math.sin(x)/x)) * window_fn(j/half_size)) / ratio;
 	}
-	for (let i = 0; i < fir_size; i++) coef[i] /= sum;
 	return coef;
 })
