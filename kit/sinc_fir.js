7b6e8c7bde504a8689d31e87b18b0f8060dbf72a
Anders Kaare Straadt <anders@straadt.dk>
2024-10-03 11:56:22 +0200

    sinc table bugs

--- kit/sinc_fir.js
+++ kit/sinc_fir.js
@@ -2,9 +2,14 @@
 	let half_size = ratio*n_zero_crossings;
 	let fir_size = 2*half_size - 1;
 	let coef = new Float32Array(fir_size);
+	let sum = 0;
+	let j;
 	for (let i = 0; i < fir_size; i++) {
-		let x = ((i-half_size+1)*Math.PI)/ratio;
-		coef[i] = ((i+1)%ratio==0) ? 0 : (x==0 ? 1 : (Math.sin(x)/x)) * window_fn(i/(fir_size-1));
+		j = (i-half_size+1);
+		let x = (j*Math.PI)/ratio;
+		coef[i] = (x==0 ? 1 : ((i+1)%ratio==0) ? 0 : (Math.sin(x)/x)) * window_fn(j/half_size);
+		sum += coef[i];
 	}
+	for (let i = 0; i < fir_size; i++) coef[i] /= sum;
 	return coef;
 })
