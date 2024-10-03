ef25e0bf544f61aac2a701d33c45f359f22c4591
Anders Kaare Straadt <anders@straadt.dk>
2024-10-03 15:22:48 +0200

    off-by-one

--- kit/sinc_downsampler.js
+++ kit/sinc_downsampler.js
@@ -9,22 +9,24 @@ creates a downsampler (A,B) that downsamples A into B.
 	return (A,B) => {
 		let n0 = n_zero_crossings*2-1;
 		let i,j,k,sum;
-		for (i=0;i<n0;i++) {
+		for (i=0;i<=n0;i++) {
 			sum=0;
 			for (j=0;j<nf;j++) {
 				k=i*ratio+j-nf;
-				sum += (k >= 0 ? A[k] : tail[tail.length+k]) * fir[j];
+				//if (!(-nf <= k && k < A.length)) throw new Error("BOUNDS(0)");
+				sum += (k >= 0 ? A[k] : tail[nf+k]) * fir[j];
 			}
 			B[i]=sum;
 		}
 		for (;i<B.length;i++) {
 			sum=0;
 			for (j=0;j<nf;j++) {
+				//if (!(0 <= (i*ratio+j-nf) && (i*ratio+j-nf) < A.length)) throw new Error("BOUNDS(1)");
 				sum += A[i*ratio+j-nf] * fir[j];
 			}
 			B[i]=sum;
 		}
 
-		tail.set(A.slice(A.length-tail.length));
+		tail.set(A.slice(A.length-nf));
 	};
 })
